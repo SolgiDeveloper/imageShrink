a8a491a9e90177a042fc43db08246ef95dab2249
Mohammad Solgi <m.solgi@apasaico.local>
2021-12-26 01:00:54 +0330

    improve app

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, Menu} = require('electron')
+const {app, BrowserWindow, Menu, globalShortcut} = require('electron')
 //set env
 process.env.NODE_ENV = 'development'
 
@@ -7,34 +7,79 @@ const isMac = process.env.NODE_ENV !== 'darwin'
 const isWin = process.env.NODE_ENV !== 'win32'
 
 let mainWindow
+let aboutWindow
 app.on('ready', ()=>{
   const mainMenu = Menu.buildFromTemplate(menu)
   Menu.setApplicationMenu(mainMenu)
+
+  globalShortcut.register('CmdOrCtrl+R', ()=> mainWindow.reload())
+  globalShortcut.register(isWin ? 'Ctrl+Shift+I': 'Command+Alt+I', ()=> mainWindow.toggleDevTools())
   mainWindow = new BrowserWindow({
     title: 'ImageShrink',
-    width: 500,
+    width: isDev ? 800 : 500,
     height: 600,
     resizable: isDev,
     icon: './assets/icons/Icon_256x256.png',
+    backgroundColor: 'green',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
     }
   })
+  if (isDev){
+    mainWindow.webContents.openDevTools()
+  }
   // mainWindow.loadURL(`file://${__dirname}/app/index.html`)
   mainWindow.loadFile('./app/index.html')
+  mainWindow.on('closed', ()=> app.quit())
 })
-
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    resizable: false,
+    icon: './assets/icons/Icon_256x256.png',
+    backgroundColor: 'green',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  })
+  aboutWindow.loadFile('./app/about.html')
+  aboutWindow.on('closed', ()=> aboutWindow = null)
+}
 const menu = [
   {
     label: 'File',
     submenu: [
       {
         label: 'Quit',
+        accelerator: isWin ? 'Ctrl+W':'Command+W',
         click: ()=> app.quit()
       }
     ]
-  }
+  },
+  {
+    label: 'Help',
+    submenu: [
+      {
+        label: 'About',
+        click:() => createAboutWindow()
+      }
+    ]
+  },
+  ...(isDev ? [
+      {
+        label: 'Developer',
+        submenu: [
+          {role: 'reload'},
+          {role: 'forcereload'},
+          {type: 'separator'},
+          {role: 'toggledevtools'},
+        ]
+      }
+    ] : [])
 ]
 
 
